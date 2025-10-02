65defad5dda42c832bf874027051d7c40620d45e
shivaaganesh3 <shivaaganesh3@gmail.com>
2025-10-02 20:20:29 +0530

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -4,44 +4,35 @@ import { Button } from '@/components/ui/button'
 import CTA from '@/components/CTA'
 import CompanionsList from '@/components/CompanionsList'
 import { recentSessions } from '@/constants'
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
-const Page = () => {
+
+const Page = async() => {
+  const companions = await getAllCompanions( {limit: 3});
+  const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
     <main>
       <h1 className='text-2xl underline'> Popular Companions</h1>
-      <section className='home-section'>
-        <CompanionCard 
-        id='123'
-        name='Neura the Brainy Explorer'
-        topic='Neural Network of the Brain'
-        subject='science'
-        duration={45}
-        color='#E5D0FF'
-        />
-        <CompanionCard 
-        id='456'
-        name='Countsy the Number Wizard'
-        topic='Derivatives & Integrals'
-        subject='maths'
-        duration={30}
-        color='#FFDA6E'
-        />
-        <CompanionCard 
-        id='789'
-        name='Verba the Vocabulary Builder'
-        topic='English Literature'
-        subject='science'
-        duration={30}
-        color='#BDE7FF'
-        />
-      </section>
-      <section className='home-section'>
-        <CompanionsList 
-          title='Recent Completed Sessions'
-          companions={recentSessions}
-          classNames='w-2/3 max-lg:w-full'
-        />
-        <CTA />
+      <section className="home-section">
+            {companions.map((companion) => (
+                <CompanionCard
+                    key={companion.id}
+                    {...companion}
+                    color={getSubjectColor(companion.subject)}
+                />
+            ))}
+
+        </section>
+
+        <section className="home-section">
+            <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessionsCompanions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+            <CTA />
       </section>
     </main>
   )
---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2a9c3177f177dcc4d6d6d0b9b3425776@o4510120161837056.ingest.de.sentry.io/4510120163541072",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
